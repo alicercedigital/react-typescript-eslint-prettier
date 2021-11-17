b565bb50e4ed3b1d26bd4426939164f3046568b1
Danilo Cunha <contato@alicercedigital.com.br>
2021-11-16 21:15:07 -0300

    alterações

--- src/components/Property/List/index.tsx
+++ src/components/Property/List/index.tsx
@@ -81,7 +81,7 @@ const PropertyList = () => {
       )}
 
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table>
           <TableHead>
             <TableRow>
               <TableCell>Tipo</TableCell>
